bdc097ac73b9b7e095a784bf3148089f450b7581
hamzaaasif <hamzaasif0987@gmail.com>
2023-09-05 04:31:23 +0500

    user module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { getEnvPath } from './config/database/env';
 
+import { TypeOrmConfigService } from './config/database/database';
+import { AppController } from 'app.controller';
+
+const envFilePath: string = getEnvPath(join(`${__dirname}`, '..'));
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath,
+      isGlobal: true,
+    }),
+    AuthModule,
+    CoreModule,
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
